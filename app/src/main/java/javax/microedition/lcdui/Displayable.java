b17da38e634c6d541e5a449d6f0c4182e260585b
Nikita <@>
2018-01-09 21:52:32 +0300

    Update copyright headers

--- app/src/main/java/javax/microedition/lcdui/Displayable.java
+++ app/src/main/java/javax/microedition/lcdui/Displayable.java
@@ -1,7 +1,7 @@
 /*
  * Copyright 2012 Kulikov Dmitriy
  * Copyright 2015-2016 Nickolay Savchenko
- * Copyright 2017 Nikita Shakarun
+ * Copyright 2017-2018 Nikita Shakarun
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
