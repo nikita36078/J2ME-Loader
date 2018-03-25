dea83127b5280004aebee2a67b95551e66d1e4e9
Nikita Shakarun <nikita36078@gmail.com>
2018-03-25 16:07:46 +0300

    Fix Immersive Mode bug

--- app/src/main/java/javax/microedition/shell/MicroActivity.java
+++ app/src/main/java/javax/microedition/shell/MicroActivity.java
@@ -244,8 +244,7 @@ public class MicroActivity extends AppCompatActivity {
 	private void hideSystemUI() {
 		if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
 			getWindow().getDecorView().setSystemUiVisibility(
-					View.SYSTEM_UI_FLAG_LAYOUT_STABLE
-							| View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+					View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
 							| View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
 							| View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
 							| View.SYSTEM_UI_FLAG_FULLSCREEN
