1300ed727cefc1a55b90d1d1c60d68073a1727bd
Nikita Shakarun <nikita36078@gmail.com>
2018-04-27 17:08:49 +0300

    Add IMEI property

--- app/src/main/java/javax/microedition/shell/ConfigActivity.java
+++ app/src/main/java/javax/microedition/shell/ConfigActivity.java
@@ -262,6 +262,7 @@ public class ConfigActivity extends BaseActivity implements View.OnClickListener
 		System.setProperty("microedition.encoding", "ISO-8859-1");
 		System.setProperty("user.home", Environment.getExternalStorageDirectory().getPath());
 		System.setProperty("com.siemens.IMEI", "000000000000000");
+		System.setProperty("device.imei", "000000000000000");
 	}
 
 	@Override
