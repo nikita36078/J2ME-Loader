e8039bc9e7f5c1d84ef053e313903f44f555137a
Nikita Shakarun <nikita36078@gmail.com>
2019-05-07 19:40:53 +0300

    Add signature validation

--- app/src/main/java/ru/playsoftware/j2meloader/EmulatorApplication.java
+++ app/src/main/java/ru/playsoftware/j2meloader/EmulatorApplication.java
@@ -16,12 +16,20 @@
 
 package ru.playsoftware.j2meloader;
 
+import android.annotation.SuppressLint;
 import android.app.Application;
 import android.content.Context;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.content.pm.Signature;
 
 import org.acra.ACRA;
 import org.acra.annotation.AcraCore;
 import org.acra.annotation.AcraDialog;
+
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.Arrays;
 
 import ru.playsoftware.j2meloader.hockeyapp.HockeySenderFactory;
 
@@ -30,9 +38,49 @@ import ru.playsoftware.j2meloader.hockeyapp.HockeySenderFactory;
 		resPositiveButtonText = R.string.report_crash, resNegativeButtonText = R.string.CANCEL_CMD,
 		resTheme = R.style.Theme_AppCompat_Dialog)
 public class EmulatorApplication extends Application {
+	private static final String[] VALID_SIGNATURES = {
+			"78EF7758720A9902F731ED706F72C669C39B765C", // GPlay
+			"289F84A32207DF89BE749481ED4BD07E15FC268F", // F-Droid
+			"FA8AA497194847D5715BAA62C6344D75A936EBA6" // Private
+	};
+
+	@SuppressWarnings("ConstantConditions")
 	@Override
 	protected void attachBaseContext(Context base) {
 		super.attachBaseContext(base);
-		if (!BuildConfig.DEBUG && !BuildConfig.FLAVOR.equals("dev")) ACRA.init(this);
+		if (isSignatureValid() && !BuildConfig.FLAVOR.equals("dev")) ACRA.init(this);
+	}
+
+	@SuppressLint("PackageManagerGetSignatures")
+	private boolean isSignatureValid() {
+		try {
+			PackageInfo info = getPackageManager().getPackageInfo(getPackageName(), PackageManager.GET_SIGNATURES);
+			for (Signature signature : info.signatures) {
+				MessageDigest md = MessageDigest.getInstance("SHA-1");
+				md.update(signature.toByteArray());
+				String sha1 = bytesToHex(md.digest());
+				if (!Arrays.asList(VALID_SIGNATURES).contains(sha1)) {
+					return false;
+				}
+			}
+		} catch (PackageManager.NameNotFoundException e) {
+			e.printStackTrace();
+		} catch (NoSuchAlgorithmException e) {
+			e.printStackTrace();
+		}
+		return true;
+	}
+
+	private String bytesToHex(byte[] bytes) {
+		final char[] hexArray = {'0', '1', '2', '3', '4', '5', '6', '7', '8',
+				'9', 'A', 'B', 'C', 'D', 'E', 'F'};
+		char[] hexChars = new char[bytes.length * 2];
+		int v;
+		for (int i = 0; i < bytes.length; i++) {
+			v = bytes[i] & 0xFF;
+			hexChars[i * 2] = hexArray[v >>> 4];
+			hexChars[i * 2 + 1] = hexArray[v & 0x0F];
+		}
+		return new String(hexChars);
 	}
 }
