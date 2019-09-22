de1f6f77d5966d2816509282dfc8034299f3f9dc
Nikita Shakarun <nikita36078@gmail.com>
2019-09-22 10:10:44 +0300

    Make player callbacks asynchronous

--- app/src/main/java/javax/microedition/media/MicroPlayer.java
+++ app/src/main/java/javax/microedition/media/MicroPlayer.java
@@ -88,7 +88,7 @@ public class MicroPlayer extends BasePlayer implements MediaPlayer.OnCompletionL
 	}
 
 	@Override
-	public void addPlayerListener(PlayerListener playerListener) {
+	public synchronized void addPlayerListener(PlayerListener playerListener) {
 		checkClosed();
 		if (!listeners.contains(playerListener) && playerListener != null) {
 			listeners.add(playerListener);
@@ -96,14 +96,16 @@ public class MicroPlayer extends BasePlayer implements MediaPlayer.OnCompletionL
 	}
 
 	@Override
-	public void removePlayerListener(PlayerListener playerListener) {
+	public synchronized void removePlayerListener(PlayerListener playerListener) {
 		checkClosed();
 		listeners.remove(playerListener);
 	}
 
-	public void postEvent(String event) {
+	public synchronized void postEvent(String event) {
 		for (PlayerListener listener : listeners) {
-			listener.playerUpdate(this, event, source.getLocator());
+			// Callbacks should be async
+			Runnable r = () -> listener.playerUpdate(this, event, source.getLocator());
+			(new Thread(r, "MIDletPlayerCallback")).start();
 		}
 	}
 
@@ -124,7 +126,7 @@ public class MicroPlayer extends BasePlayer implements MediaPlayer.OnCompletionL
 	}
 
 	@Override
-	public void realize() throws MediaException {
+	public synchronized void realize() throws MediaException {
 		checkClosed();
 
 		if (state == UNREALIZED) {
@@ -140,7 +142,7 @@ public class MicroPlayer extends BasePlayer implements MediaPlayer.OnCompletionL
 	}
 
 	@Override
-	public void prefetch() throws MediaException {
+	public synchronized void prefetch() throws MediaException {
 		checkClosed();
 
 		if (state == UNREALIZED) {
@@ -191,7 +193,7 @@ public class MicroPlayer extends BasePlayer implements MediaPlayer.OnCompletionL
 	}
 
 	@Override
-	public void deallocate() {
+	public synchronized void deallocate() {
 		stop();
 
 		if (state == PREFETCHED) {
@@ -207,7 +209,7 @@ public class MicroPlayer extends BasePlayer implements MediaPlayer.OnCompletionL
 	}
 
 	@Override
-	public void close() {
+	public synchronized void close() {
 		if (state != CLOSED) {
 			player.release();
 		}
