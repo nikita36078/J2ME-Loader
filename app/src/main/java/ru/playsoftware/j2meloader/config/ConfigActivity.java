7451d07348f4cda2c88dc8ae8c1b3de8012d4a5f
woesss <yurets.droid@gmail.com>
2020-04-12 23:48:10 +0300

    Improve config settings

--- app/src/main/java/ru/playsoftware/j2meloader/config/ConfigActivity.java
+++ app/src/main/java/ru/playsoftware/j2meloader/config/ConfigActivity.java
@@ -492,13 +492,19 @@ public class ConfigActivity extends BaseActivity implements View.OnClickListener
 		cxVKFeedback.setChecked(params.getBoolean(("VirtualKeyboardFeedback"), false));
 		cxVKForceOpacity.setChecked(params.getBoolean(("VirtualKeyboardForceOpacity"), false));
 		cxTouchInput.setChecked(params.getBoolean(("TouchInput"), true));
-		tfFpsLimit.setText(Integer.toString(params.getInt("FpsLimit", 0)));
+		int fpsLimit = params.getInt("FpsLimit", 0);
+		if (fpsLimit > 0) {
+			tfFpsLimit.setText(Integer.toString(fpsLimit));
+		}
 
 		spVKType.setSelection(params.getInt("VirtualKeyboardType", 0));
 		spLayout.setSelection(params.getInt("Layout", 0));
 		spButtonsShape.setSelection(params.getInt("ButtonShape", VirtualKeyboard.OVAL_SHAPE));
 		sbVKAlpha.setProgress(params.getInt("VirtualKeyboardAlpha", 64));
-		tfVKHideDelay.setText(Integer.toString(params.getInt("VirtualKeyboardDelay", -1)));
+		int vkHideDelay = params.getInt("VirtualKeyboardDelay", -1);
+		if (vkHideDelay > 0) {
+			tfVKHideDelay.setText(Integer.toString(vkHideDelay));
+		}
 		tfVKBack.setText(Integer.toHexString(
 				params.getInt("VirtualKeyboardColorBackground", 0xD0D0D0)).toUpperCase());
 		tfVKFore.setText(Integer.toHexString(
