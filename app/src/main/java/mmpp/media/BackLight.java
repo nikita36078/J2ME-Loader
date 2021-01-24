70c72737b8fc6aa30972b7e17e306a6501ae9614
Nikita Shakarun <nikita36078@gmail.com>
2021-01-24 11:31:17 +0300

    Add LG BackLight API

---
+++ app/src/main/java/mmpp/media/BackLight.java
@@ -0,0 +1,36 @@
+/*
+ * Copyright 2021 Nikita Shakarun
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package mmpp.media;
+
+public class BackLight {
+	public static int getColor() {
+		return 0;
+	}
+
+	public static int numColors() {
+		return 0;
+	}
+
+	public static void off() {
+	}
+
+	public static void on(int timeout) {
+	}
+
+	public static void setColor(int RGB) {
+	}
+}
