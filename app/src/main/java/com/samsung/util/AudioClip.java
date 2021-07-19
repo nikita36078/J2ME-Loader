421f326e10ed61f49ed6fbf2ae08531f18f060c1
woesss <yurets.droid@gmail.com>
2021-07-19 10:46:26 +0300

    SamsungAPI: fix exception on play after stop

--- app/src/main/java/com/samsung/util/AudioClip.java
+++ app/src/main/java/com/samsung/util/AudioClip.java
@@ -16,6 +16,8 @@
 
 package com.samsung.util;
 
+import android.util.Log;
+
 import java.io.ByteArrayInputStream;
 import java.io.IOException;
 import java.io.InputStream;
@@ -23,29 +25,45 @@ import java.io.InputStream;
 import javax.microedition.media.Manager;
 import javax.microedition.media.MediaException;
 import javax.microedition.media.Player;
-import javax.microedition.util.ContextHolder;
+import javax.microedition.media.control.VolumeControl;
+import javax.microedition.shell.AppClassLoader;
 
 public class AudioClip {
-	public static final int TYPE_MIDI = 3;
+	private static final String TAG = AudioClip.class.getName();
 	public static final int TYPE_MMF = 1;
 	public static final int TYPE_MP3 = 2;
+	public static final int TYPE_MIDI = 3;
+
+	private static final String[] FORMATS = {"audio/mmf", "audio/mp3", "audio/midi"};
 
 	private Player player;
 
-	public AudioClip(int type, java.lang.String filename) throws IOException {
-		try {
-			InputStream stream = ContextHolder.getResourceAsStream(null, filename);
-			player = Manager.createPlayer(stream, "audio/midi");
-		} catch (IOException e) {
-			e.printStackTrace();
+	public AudioClip(int type, String filename) throws IOException {
+		if (type < 1 || type > 3) {
+			throw new IllegalArgumentException();
 		}
+		if (filename == null) {
+			throw new NullPointerException();
+		}
+		InputStream stream = AppClassLoader.getResourceAsStream(null, filename);
+		player = Manager.createPlayer(stream, FORMATS[type - 1]);
 	}
 
 	public AudioClip(int type, byte[] audioData, int audioOffset, int audioLength) {
+		if (type < 1 || type > 3) {
+			throw new IllegalArgumentException();
+		}
+		if (audioData == null) {
+			throw new NullPointerException();
+		}
+		if (audioOffset < 0 || audioLength < 0 || audioOffset + audioLength > audioData.length) {
+			throw new ArrayIndexOutOfBoundsException();
+		}
+		ByteArrayInputStream stream = new ByteArrayInputStream(audioData, audioOffset, audioLength);
 		try {
-			player = Manager.createPlayer(new ByteArrayInputStream(audioData), "audio/mmf");
+			player = Manager.createPlayer(stream, FORMATS[type - 1]);
 		} catch (IOException e) {
-			e.printStackTrace();
+			Log.e(TAG, "AudioClip: ", e);
 		}
 	}
 
@@ -57,11 +75,14 @@ public class AudioClip {
 		try {
 			player.stop();
 		} catch (MediaException e) {
-			e.printStackTrace();
+			Log.e(TAG, "pause: ", e);
 		}
 	}
 
 	public void play(int loop, int volume) {
+		if (loop < 0 || loop > 255 || volume < 0 || volume > 5) {
+			throw new IllegalArgumentException();
+		}
 		try {
 			if (loop == 0) {
 				loop = -1;
@@ -70,9 +91,10 @@ public class AudioClip {
 				player.stop();
 			}
 			player.setLoopCount(loop);
+			((VolumeControl) player).setLevel(volume * 20);
 			player.start();
 		} catch (MediaException e) {
-			e.printStackTrace();
+			Log.e(TAG, "play: ", e);
 		}
 	}
 
@@ -80,11 +102,15 @@ public class AudioClip {
 		try {
 			player.start();
 		} catch (MediaException e) {
-			e.printStackTrace();
+			Log.e(TAG, "resume: ", e);
 		}
 	}
 
 	public void stop() {
-		player.close();
+		try {
+			player.stop();
+		} catch (MediaException e) {
+			Log.e(TAG, "stop: ", e);
+		}
 	}
 }
