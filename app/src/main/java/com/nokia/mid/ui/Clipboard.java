4fe8269becdcea8e1e5557db75cf9eba4db0b077
Shinovon <arman200711@gmail.com>
2021-10-10 15:34:58 +0500

    symbian^3 nokiaui Clipboard

---
+++ app/src/main/java/com/nokia/mid/ui/Clipboard.java
@@ -0,0 +1,36 @@
+package com.nokia.mid.ui;
+
+import static android.content.ClipDescription.MIMETYPE_TEXT_PLAIN;
+
+import android.content.ClipData;
+import android.content.ClipboardManager;
+import android.content.Context;
+
+import javax.microedition.lcdui.Display;
+
+public abstract class Clipboard {
+	private static ClipboardManager clipboardmgr;
+
+	public static void copyToClipboard(String text) {
+		if(clipboardmgr == null) {
+			clipboardmgr = (ClipboardManager) Display.getDisplay(null).getCurrent().getDisplayableView().getContext().getSystemService(Context.CLIPBOARD_SERVICE);
+		}
+		ClipData clip = ClipData.newPlainText("", text);
+		clipboardmgr.setPrimaryClip(clip);
+	}
+
+	public static String copyFromClipboard() {
+		if(clipboardmgr == null) {
+			clipboardmgr = (ClipboardManager) Display.getDisplay(null).getCurrent().getDisplayableView().getContext().getSystemService(Context.CLIPBOARD_SERVICE);
+		}
+		String text = null;
+		if (!clipboardmgr.hasPrimaryClip()) {
+			return "";
+		}
+		if (!clipboardmgr.getPrimaryClipDescription().hasMimeType(MIMETYPE_TEXT_PLAIN)) {
+			return "";
+		}
+		ClipData.Item item = clipboardmgr.getPrimaryClip().getItemAt(0);
+		return item.getText().toString();
+	}
+}
