962ac338ec8bc40a04135e0dcb771b639fd6abb3
Nikita Shakarun <nikita36078@gmail.com>
2023-01-01 14:37:20 +0300

    dexer: Use epoch time format for jar files

--- dexlib/src/main/java/com/android/dx/cf/direct/ClassPathOpener.java
+++ dexlib/src/main/java/com/android/dx/cf/direct/ClassPathOpener.java
@@ -276,7 +276,7 @@ public class ClassPathOpener {
                     bytes = new byte[0];
                 }
 
-                any |= consumer.processFileBytes(path, one.getLastModifiedTime(), bytes);
+                any |= consumer.processFileBytes(path, one.getLastModifiedTimeEpoch(), bytes);
             }
         }
 
