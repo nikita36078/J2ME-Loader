4866947d45354ccc13c06d904029bebc32c4968d
Arman Jussupgaliyev <43963888+shinovon@users.noreply.github.com>
2023-05-24 23:30:24 +0500

    Fix getGameAction (#999)
    * Fix getGameAction
    
    * Simplify
    
    ---------
    
    Co-authored-by: Nikita Shakarun <nikita36078@gmail.com>

--- app/src/main/java/javax/microedition/lcdui/Canvas.java
+++ app/src/main/java/javax/microedition/lcdui/Canvas.java
@@ -237,12 +237,7 @@ public abstract class Canvas extends Displayable {
 	}
 
 	public int getGameAction(int keyCode) {
-		int res = KeyMapper.getGameAction(keyCode);
-		if (res != Integer.MAX_VALUE) {
-			return res;
-		} else {
-			throw new IllegalArgumentException("unknown keycode " + keyCode);
-		}
+		return KeyMapper.getGameAction(keyCode);
 	}
 
 	public String getKeyName(int keyCode) {
