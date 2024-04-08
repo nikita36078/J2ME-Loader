c97ced4c407f8f6b03103e36ed11ec989019ca8a
Shinovon <arman200711@gmail.com>
2024-04-08 16:27:28 +0500

    lcdui x2

--- app/src/main/java/javax/microedition/lcdui/commands/ScreenSoftBar.java
+++ app/src/main/java/javax/microedition/lcdui/commands/ScreenSoftBar.java
@@ -1,5 +1,6 @@
 /*
  *  Copyright 2019-2022 Yury Kharchenko
+ *  Copyright 2023 Arman Jussupgaliyev
  *
  *  Licensed under the Apache License, Version 2.0 (the "License");
  *  you may not use this file except in compliance with the License.
