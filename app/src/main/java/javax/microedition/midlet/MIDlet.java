5f197a6fe67a358f892f500b467f658fc584772f
Shinovon <arman200711@gmail.com>
2024-04-08 16:08:43 +0500

    javaapp: protocol

--- app/src/main/java/javax/microedition/midlet/MIDlet.java
+++ app/src/main/java/javax/microedition/midlet/MIDlet.java
@@ -2,6 +2,7 @@
  * Copyright 2012 Kulikov Dmitriy
  * Copyright 2015-2016 Nickolay Savchenko
  * Copyright 2017-2018 Nikita Shakarun
+ * Copyright 2022-2023 Arman Jussupgaliyev
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
